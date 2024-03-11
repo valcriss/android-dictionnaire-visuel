fc1e8e84854cc93cae40c29092be8635c26e5f69
Daniel Silvestre <danielsilvestre.work@gmail.com>
2024-03-11 01:48:52 +0100

    update

---
+++ app/src/main/java/fr/danielsilvestre/dictionnairevisuel/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.danielsilvestre.dictionnairevisuel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainactivity)
+    }
+}
